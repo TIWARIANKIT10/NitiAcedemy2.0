500e9b07d6390fa65eaeeab934a9dd6e827a9ff3
TIWARIANKIT10 <00tiwariankit00@gmail.com>
2025-10-26 13:38:51 +0545

    url updated in form post method

--- frontend/components/section/Form.tsx
+++ frontend/components/section/Form.tsx
@@ -34,7 +34,7 @@ export default function RegistrationPage() {
     }
 
     try {
-      const res = await fetch("http://localhost:4080/api/user", {
+      const res = await fetch("https://niti-acedemy2-0.vercel.app/api/user", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
